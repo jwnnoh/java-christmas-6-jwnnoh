1227ef25e8baf46ca9172be7da32205801163c17
노정완 <jeongwan.noh@gmail.com>
2023-11-11 18:16:40 +0900

    feat: 에러 메세지 출력 메서드 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -4,6 +4,16 @@ import christmas.view.constants.ConstantMessage;
 
 public class OutputView {
 
+    public void printErrorFromDateMessage() {
+        System.out.println(ConstantMessage.ERROR_PREFIX.getMessage() +
+                ConstantMessage.ERROR_DATE.getMessage());
+    }
+
+    public void printErrorFromOrderMessage() {
+        System.out.println(ConstantMessage.ERROR_PREFIX.getMessage() +
+                ConstantMessage.ERROR_ORDER.getMessage());
+    }
+
     public void printWelcomeMessage() {
         System.out.println(ConstantMessage.WELCOME.getMessage());
     }
--- src/main/java/christmas/view/constants/ConstantMessage.java
+++ src/main/java/christmas/view/constants/ConstantMessage.java
@@ -1,6 +1,9 @@
 package christmas.view.constants;
 
 public enum ConstantMessage {
+    ERROR_PREFIX("[ERROR] "),
+    ERROR_DATE("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
+    ERROR_ORDER("유효하지 않은 주문입니다. 다시 입력해 주세요."),
     WELCOME("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."),
     REQUEST_DATE_OF_VISIT("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
     private final String message;
