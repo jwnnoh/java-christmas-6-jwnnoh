222e5a922a20e376fde3193bb26d66bbb80fc536
노정완 <jeongwan.noh@gmail.com>
2023-11-12 15:09:53 +0900

    fix: 증정 메뉴 최소 충족 금액이 포함되지 않은 오류 수정

--- src/main/java/christmas/domain/service/Giveaway.java
+++ src/main/java/christmas/domain/service/Giveaway.java
@@ -6,7 +6,7 @@ import christmas.domain.constants.Menu;
 public class Giveaway {
     private static final int AMOUNT_REQUIRED = 120000;
     public String checkGiveaway(int purchaseAmount) {
-        if (purchaseAmount > AMOUNT_REQUIRED) {
+        if (purchaseAmount >= AMOUNT_REQUIRED) {
             return Menu.CHAMPAGNE.getName();
         }
         return Constant.BENEFIT_UNAVAILABLE.getMessage();
