0c96310a58a9ab5c5c5e9c38d928c60e389c255c
노정완 <jeongwan.noh@gmail.com>
2023-11-12 15:59:20 +0900

    refactor: 금액을 포매팅하는 메서드를 분리

---
+++ src/main/java/christmas/domain/service/DecimalFormatFormatter.java
@@ -0,0 +1,12 @@
+package christmas.domain.service;
+
+import java.text.DecimalFormat;
+
+public class DecimalFormatFormatter {
+    private static final String REWARD_FORMAT = "###,###";
+
+    public String returnDecimalFormatAmount(int amount) {
+        DecimalFormat decimalFormat = new java.text.DecimalFormat(REWARD_FORMAT);
+        return decimalFormat.format(amount);
+    }
+}
