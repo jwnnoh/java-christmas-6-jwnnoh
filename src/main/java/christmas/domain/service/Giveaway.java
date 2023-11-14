149878999793bc01c6f1037f28186c225b47f024
노정완 <jeongwan.noh@gmail.com>
2023-11-14 23:32:25 +0900

    fix: 띄어쓰기가 두번 되는 오류 수정

--- src/main/java/christmas/domain/service/Giveaway.java
+++ src/main/java/christmas/domain/service/Giveaway.java
@@ -5,7 +5,7 @@ import christmas.domain.constants.Menu;
 import static christmas.domain.constants.Menu.CHAMPAGNE;
 
 public class Giveaway {
-    public static final String GIVEAWAY_MENU_AMOUNT = " 1";
+    public static final int GIVEAWAY_MENU_AMOUNT = 1;
     public static final int AMOUNT_REQUIRED = 120000;
     public static final Menu GIVEAWAY_MENU = CHAMPAGNE;
 
