bcf8b6afe920108f727dcbdc46cf94816f6499f8
노정완 <jeongwan.noh@gmail.com>
2023-11-14 21:57:38 +0900

    test: 입력값의 숫자 여부를 확인, 반환하는 로직을 검증하는 테스트 코드 작성

---
+++ src/test/java/christmas/domain/validation/ValidatorTest.java
@@ -0,0 +1,67 @@
+package christmas.domain.validation;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ValidatorTest {
+    private Validator validator;
+    @BeforeEach
+    void setUp() {
+        validator = new Validator();
+    }
+
+    @DisplayName("숫자 입력시 정상 통과한다.")
+    @Test
+    public void testValidateInputIntegerWithInteger() {
+        // Given
+        String quantity = "19";
+
+        // When, Then
+        assertThatCode(() -> validator.validateInputInteger(quantity))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("공백, 문자, 빈 값 입력시 예외를 발생시킨다.")
+    @Test
+    public void testValidateInputIntegerWithoutString() {
+        // Given
+        List<String> quantities = List.of("five", " ", "");
+
+        // When, Then
+        for (String quantity : quantities) {
+            assertThatThrownBy(() -> validator.validateInputInteger(quantity))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @DisplayName("주문 메뉴를 반환하는 맵에 중복된 메뉴 주문이 없으면 정상 통과한다.")
+    @Test
+    public void testValidateDuplicatesWithNonDuplicates() {
+        // Given
+        Map<String, Integer> splitMenu = new HashMap<>();
+        splitMenu.put("초코케이크", 1);
+
+        // When, Then
+        assertThatCode(() -> validator.validateDuplicates("제로콜라", splitMenu))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("주문 메뉴를 반환하는 맵에 중복된 메뉴 주문이 있으면 예외를 발생시킨다.")
+    @Test
+    public void testValidateDuplicatesWithDuplicates() {
+        // Given
+        Map<String, Integer> splitMenu = new HashMap<>();
+        splitMenu.put("초코케이크", 2);
+
+        // When, Then
+        assertThatThrownBy(() -> validator.validateDuplicates("초코케이크", splitMenu))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
